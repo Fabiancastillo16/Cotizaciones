77690cac226713fa26d37871302e8756db8e16bd
Fabiancastillo16 <castillorojas8@gmail.com>
2018-01-02 12:01:07 -0300

    commit inicial

---
+++ Migrations/20171229215354_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace cotizaciones.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Materno = table.Column<string>(nullable: true),
+                    Nombre = table.Column<string>(nullable: true),
+                    Paterno = table.Column<string>(nullable: true),
+                    Rut = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cotizaciones",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    clienteId = table.Column<int>(nullable: true),
+                    descripcion = table.Column<string>(nullable: true),
+                    monto = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cotizaciones", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Cotizaciones_Personas_clienteId",
+                        column: x => x.clienteId,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cotizaciones_clienteId",
+                table: "Cotizaciones",
+                column: "clienteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cotizaciones");
+
+            migrationBuilder.DropTable(
+                name: "Personas");
+        }
+    }
+}
---
+++ Models/Cotizacion.cs
@@ -0,0 +1,25 @@
+/// <summary>
+/// Archivo donde se definen las clases del Dominio del problema.
+/// </summary>
+namespace Cotizaciones.Models {
+
+    /// <summary>
+    /// Clase que representa a una cotizacion en el Sistema.
+    /// </summary>
+    /// <remarks>
+    /// Esta clase pertenece al modelo del Dominio y posee las siguientes restricciones:
+    /// - No permite valores null en sus atributos.
+    /// </remarks>
+    public class Cotizacion
+    {
+        public int id { get; set; }
+
+        public Persona cliente { get; set; }
+
+        public string descripcion { get; set; }
+
+        public double monto { get; set; }
+
+        
+    }
+}
