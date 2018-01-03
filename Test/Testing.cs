66a06cb23388874f8659be180ceb84f719f94e68
Fabiancastillo16 <castillorojas8@gmail.com>
2018-01-03 14:21:41 -0300

    update clase testing

--- Test/Testing.cs
+++ Test/Testing.cs
@@ -1,17 +1,78 @@
-using Xunit;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using Cotizaciones.Data;
-using System.Linq;
-using System;
+using Microsoft.Extensions.Logging;
+using Xunit;
 using System.Collections.Generic;
-using Cotizaciones.Models;
+using System;
 
-namespace Cotizaciones.Data{
+namespace Cotizaciones.Models 
+{
+    public class MainServicioTest : IDisposable  {
 
-    public class MainDataTest  {
+        IMainServicio Servicio {get; set;}
 
-        
+        ILogger Logger {get; set;}
+
+        public MainServicioTest(){
+
+            // Logger Factory
+            ILoggerFactory loggerFactory = new LoggerFactory().AddConsole().AddDebug();
+            Logger = loggerFactory.CreateLogger<MainServicioTest>();
+
+            Logger.LogInformation("Initializing Test ..");
+
+            // SQLite en memoria
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            // Opciones de inicializacion del CotizacionesContext
+            var options = new DbContextOptionsBuilder<CotizacionesContext>()
+            .UseSqlite(connection)
+            .Options;
+
+            // inicializacion del CotizacionesContext
+            CotizacionesContext backend = new CotizacionesContext(options);
+            // Crear la base de datos
+            backend.Database.EnsureCreated();
+
+            // Servicio a probar
+            Servicio = new MainServicio(backend, loggerFactory);
+
+            Logger.LogInformation("Initialize Test ok.");
+
+        }
+        [Fact]
+        public void InitializeTest()
+        {
+            Logger.LogInformation("Testing IMainService.Initialize() ..");
+            Servicio.Initialice();
+
+            // No se puede inicializar 2 veces
+            Assert.Throws<Exception>( () => { Servicio.Initialice(); });
+
+            // Personas en la BD
+            List<Persona> personas = Servicio.Personas();
+
+            // Debe ser !=  de null
+            Assert.True(personas != null);
+
+            // Debe haber 5 persona
+            Assert.True(personas.Count == 5);
+
+            // Print de la persona
+            foreach(Persona persona in personas) {
+                Logger.LogInformation("Persona: {0}", persona);
+            }
+
+            Logger.LogInformation("Test IMainService.Initialize() ok");
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
+
+
